e28271cb7fcbb258336118373555c5b5ecb0c1ae
Yaarashiran <yaara.shiran@gmail.com>
2025-03-06 21:15:15 +0200

    Update thriftiestrial.cs

--- thriftiestrial.cs
+++ thriftiestrial.cs
@@ -7,7 +7,7 @@
 body {
     margin: 0;
     padding: 0;
-    font-family: Arial, sans-serif; /* Optional: Set a font */
+    font-family: Arial, sans-serif; /* Optional: Set a font and discover page */
 }
 
 /* Style for the title */
